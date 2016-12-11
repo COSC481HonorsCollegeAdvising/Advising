e35bbb43f15e8adc6a26c036e99817255bd2a967
William Gillespie <wgilles1@emich.edu>
2016-12-11 18:58:30 -0500

    Added an import to JQuery and the section-detail-helper-funcs.js helper function file.  Got rid of the static fields.  On create, this will get the information from the server.  It relies on the crn session variable to be set.

--- prototype/section-details.php
+++ prototype/section-details.php
@@ -3,6 +3,9 @@
   <link rel="stylesheet" type="text/css" href="./CSS/foundation.css">
   <link rel="stylesheet" type="text/css" href="./CSS/foundation.min.css">
   <link rel="stylesheet" type="text/css" href="./CSS/global.css">
+	<script src="./JS/jquery-3.1.1.min.js"></script>
+	<script src="section-detail-helper-funcs.js"></script>
+
 </head>
   <body>
     <div id="container" class="row">
@@ -12,7 +15,7 @@
     <div>
       <div class="page">
 			<span><b>Section Details</b></span>
-			<table class="add" style="margin-top: 20px; margin-bottom: 40px;">
+			<table id="section_detail_table" class="add" style="margin-top: 20px; margin-bottom: 40px;">
 			  <tr>
 				<th>Prefix</th>
 				<th>Course No.</th>
@@ -22,18 +25,9 @@
 				<th>Registered</th>
 				<th>Capacity</th>
 			  </tr>
-			  <tr>
-				<td>COSC</td>
-				<td>111</td>
-				<td>18792</td>
-				<td>Yes</td>
-				<td>4</td>
-				<td>0</td>
-				<td>20</td>
-			  </tr>
 			</table>
         <span><b>Registered Students</b></span>
-        <table class="add" style="margin-top: 20px;">
+        <table id="students_advised_table" class="add" style="margin-top: 20px;">
           <tr>
             <th>First Name</th>
             <th>Last Name</th>
@@ -41,39 +35,17 @@
             <th>Advisor</th>
             <th>Date</th>
           </tr>
-          <tr>
-            <td>Cameron</td>
-            <td>Copland</td>
-            <td>E09090909</td>
-            <td>John Feldkamp</td>
-            <td>10/10/2016</td>
-          </tr>
-          <tr>
-            <td>John</td>
-            <td>Davis</td>
-            <td>E09090999</td>
-            <td>Krish Narayanan</td>
-            <td>10/12/2016</td>
-          </tr>
-          <tr>
-            <td>Will</td>
-            <td>Gillespie</td>
-            <td>E09089102</td>
-            <td>Katie Augustaitis</td>
-            <td>10/12/2016</td>
-          </tr>
-          <tr>
-            <td>Christina</td>
-            <td>Spalding</td>
-            <td>E01010392</td>
-            <td>John Feldkamp</td>
-            <td>10/14/2016</td>
-          </tr>
         </table>
       </div>
       <div style="margin-top: 10px;">
         <button onclick="window.location.href='course-overview.php'">Go Back</button>
       </div>
     </div>
+
+		<script>
+			fetch_section_details();
+			fetch_students_advised_for_section();
+		</script>
+
   </body>
 </html>
